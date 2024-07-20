396fb43e5bed7b7fdeb539c10684ccc11bc4119b
Maksim <i@mpozdnjakov.ru>
2024-07-20 20:44:32 +0300

    Initial commit

---
+++ app/src/main/java/com/pozmaxpav/cinemaopinion/App.kt
@@ -0,0 +1,7 @@
+package com.pozmaxpav.cinemaopinion
+
+import android.app.Application
+
+class App: Application() {
+
+}
---
+++ app/src/main/java/com/pozmaxpav/cinemaopinion/presentation/screens/MainScreen.kt
@@ -0,0 +1,18 @@
+package com.pozmaxpav.cinemaopinion.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.pozmaxpav.cinemaopinion.ui.theme.CinemaOpinionTheme
+
+@Composable
+fun MainScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    CinemaOpinionTheme {
+        MainScreen()
+    }
+}
