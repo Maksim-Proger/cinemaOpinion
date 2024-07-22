0efd8dab7bcb6d50e470539d4b084fe09292d503
Maksim <i@mpozdnjakov.ru>
2024-07-22 15:44:19 +0300

    5 commit

--- app/src/main/java/com/pozmaxpav/cinemaopinion/utilits/funs.kt
+++ app/src/main/java/com/pozmaxpav/cinemaopinion/utilits/funs.kt
@@ -1,2 +1,26 @@
 package com.pozmaxpav.cinemaopinion.utilits
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AccountListItem(icon: ImageVector, title: String) {
+    Row(
+        modifier = Modifier.padding(horizontal = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = Modifier.padding(end = 15.dp),
+            imageVector = icon,
+            contentDescription = title
+        )
+        Text(text = title)
+    }
+}
