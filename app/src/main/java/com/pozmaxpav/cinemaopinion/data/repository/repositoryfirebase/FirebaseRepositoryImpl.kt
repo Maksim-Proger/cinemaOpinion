33c214f199d7d82220a81452043d7284f72adc30
Maksim <i@mpozdnjakov.ru>
2024-10-12 00:34:35 +0300

    67 commit

--- app/src/main/java/com/pozmaxpav/cinemaopinion/data/repository/repositoryfirebase/FirebaseRepositoryImpl.kt
+++ app/src/main/java/com/pozmaxpav/cinemaopinion/data/repository/repositoryfirebase/FirebaseRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.pozmaxpav.cinemaopinion.data.repository.repositoryfirebase
 
+import android.util.Log
 import com.google.firebase.database.DatabaseReference
 import com.pozmaxpav.cinemaopinion.data.api.models.modelfirebase.FilmData
 import com.pozmaxpav.cinemaopinion.data.mappers.toData
@@ -21,12 +22,25 @@ class FirebaseRepositoryImpl @Inject constructor(
         } ?: throw Exception("Failed to generate key")
     }
 
-    override suspend fun removeMovie(title: String) { // TODO: Надо проверить работает ли.
-        val snapshot =
-            databaseReference.child("list_movies").orderByChild("titleFilm").equalTo(title).get()
-                .await()
-        for (filmSnapshot in snapshot.children) {
-            filmSnapshot.ref.removeValue().await()
+    override suspend fun removeMovie(title: String) {
+        val titleToRemove = title.trim()
+        try {
+            val snapshot =
+                databaseReference
+                    .child("list_movies")
+                    .orderByChild("titleFilm")
+                    .equalTo(titleToRemove)
+                    .get()
+                    .await()
+
+            if (snapshot.exists() && snapshot.hasChildren()) {
+                // Проходим по всем найденным элементам
+                for (filmSnapshot in snapshot.children) {
+                    filmSnapshot.ref.removeValue().await()
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("RemoveMovie", "Error: ${e.message}")
         }
     }
 
--- app/src/main/java/com/pozmaxpav/cinemaopinion/presentation/screens/mainScreens/ListSelectedGeneralMovies.kt
+++ app/src/main/java/com/pozmaxpav/cinemaopinion/presentation/screens/mainScreens/ListSelectedGeneralMovies.kt
@@ -1,5 +1,8 @@
 package com.pozmaxpav.cinemaopinion.presentation.screens.mainScreens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -14,16 +17,24 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
@@ -31,6 +42,10 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.pozmaxpav.cinemaopinion.R
 import com.pozmaxpav.cinemaopinion.presentation.viewModel.FirebaseViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun ListSelectedGeneralMovies(
@@ -64,26 +79,61 @@ fun ListSelectedGeneralMovies(
                 contentPadding = PaddingValues(10.dp)
             ) {
                 items(listMovies) { movie ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                        colors = CardDefaults.cardColors(
-                            containerColor = MaterialTheme.colorScheme.secondary,
-                            contentColor = MaterialTheme.colorScheme.onSecondary
+
+                    var isVisible by remember { mutableStateOf(true) } // Состояние видимости
+
+                    AnimatedVisibility(
+                        visible = isVisible,
+                        exit = slideOutHorizontally(
+                            targetOffsetX = { -it }, // Уходит влево
+                            animationSpec = tween(durationMillis = 300) // Длительность анимации
                         )
                     ) {
                         Row(
                             modifier = Modifier
-                                .padding(16.dp)
+                                .fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Text(
-                                text = movie.titleFilm,
-                                style = MaterialTheme.typography.bodyLarge
-                            )
+                            Card(
+                                modifier = Modifier
+                                    .weight(0.9f)
+                                    .wrapContentHeight(),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.secondary,
+                                    contentColor = MaterialTheme.colorScheme.onSecondary
+                                )
+                            ) {
+                                Row(
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                ) {
+                                    Text(
+                                        text = movie.titleFilm,
+                                        style = MaterialTheme.typography.bodyLarge
+                                    )
+                                }
+                            }
+                            Spacer(modifier = Modifier.padding(horizontal = 10.dp))
+
+                            IconButton(
+                                modifier = Modifier
+                                    .weight(0.1f),
+                                onClick = {
+                                    isVisible = false // Скрываем элемент перед удалением
+                                    CoroutineScope(Dispatchers.Main).launch {
+                                        delay(300)
+                                        viewModel.removeMovie(movie.titleFilm)
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Close,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            }
                         }
                     }
-
                     Spacer(Modifier.padding(5.dp))
                 }
             }
